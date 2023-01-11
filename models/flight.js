040b07e34c04136a60e4f00a7b5ea03b291462fb
Juan Garcia <juanmgarcia098@gmail.com>
2023-01-10 20:44:17 -0400

    Add models and config

---
+++ models/flight.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+// optional shortcut to the mongoose.Schema class
+
+const Schema = mongoose.Schema
+	
+const flightSchema = new Schema({
+  airline: String,
+  airport: Number,
+  flightNo: String,
+  departs: Date,
+})
